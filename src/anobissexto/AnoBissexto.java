697290ce290664ddc2cca8a31e49600dd3e11995
Michael Robert Coitim <michaelcoitim1@gmail.com>
2021-06-09 23:31:46 -0300

    Adcionando a lógica  e impimindo o resultado no console

--- src/anobissexto/AnoBissexto.java
+++ src/anobissexto/AnoBissexto.java
@@ -5,6 +5,8 @@
  */
 package anobissexto;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Michael Coitim 
@@ -15,7 +17,35 @@ public class AnoBissexto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Calendar ano = Calendar.getInstance();
+        int anoAtual = ano.getWeekYear();
+        
+            // atribuirções de testes 
+         //anoAtual = 2000;
+            
+         
+         // arry para mostras os dias e os meses
+        String mes[]={"jan","fer","mar","abr","mai","jun",
+            "jul","ago","set","out","nov","dez"};
+        int tod[]= {31,28,30,31,30,31,31,31,30,31,30,31};
+        
+        // a logica para indentificar o ano Bissexto
+        // verificar se o ano é em primeiro caso divisivel por 4 excluindo os que são divisiveis por 100
+        // segundo caso os anos que são divisiveis por 4 , 100 e 400 
+         if (  (anoAtual%4==0 && anoAtual % 100 !=0)
+                 ||(anoAtual %4 ==0 && anoAtual % 100==0 && anoAtual%400==0)){
+               
+                    System.out.println("Ano "+anoAtual+  " é Bisesto");
+                    tod[1]=29;
+            }else{
+             System.out.println("Ano "+anoAtual);
+         }
+        
+         // percorendo os arrays e mostrando os meses o seus dias 
+        for (int c =0; c< mes.length; c++){
+           
+            System.out.println("O mês  de "+mes[c]+" tem "+tod[c] + " dias");
+      }
     }
     
 }
